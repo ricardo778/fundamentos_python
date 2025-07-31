792f56da9c9d0c6cef413eaf19ec864a8371ceb4
av164702gmail.com <av164702gmail.com>
2025-07-31 13:44:47 -0500

    Seccion 1: LAB  Trabajando con la función print()

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Laboratorio 1 - Uso de print()
+
+- Usé `print("Hola mundo")` y funcionó bien.
+- Probé `print('Ricardo')` y también funcionó.
+- Intenté `print(Ricardo)` sin comillas y dio error porque no existe la variable.
+- Escribí `print"Ricardo"` y `print'Ricardo'` sin paréntesis y ambos
+![alt text](../fundamentos%20python/img/seccion1.png)
---
+++ source/seccion 1/Trabajando con la función print().py
@@ -0,0 +1,10 @@
+print("Hola mundo")
+#se uso el print para mostrar el mensaje "Hola mundo" y funciona correctamente
+print('Ricardo')
+#se uso el print para mostrar el mensaje "Ricardo" y funciona correctamente
+print(Ricardo)
+#se uso el print para mostrar el mensaje Ricardo sin comillas pero arroja un error porque no se definio la variable Ricardo
+print"Ricardo"
+#se uso el print para mostrar el mensaje "Ricardo" pero arroja un error porque falta el parentesis
+print'Ricardo'
+#se uso el print para mostrar el mensaje "Ricardo" pero arroja un error porque falta el parentesis
