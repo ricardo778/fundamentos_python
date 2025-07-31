83be74fe9a3884ad3d4b356ab1fb38c49198fa5c
av164702gmail.com <av164702gmail.com>
2025-07-31 15:17:31 -0500

    seccion 1: LAB  Dando formato a la salida

--- readme.md
+++ readme.md
@@ -1,15 +1,28 @@
+##SECCIÓN 1
+
 ## Laboratorio 1 - Uso de print()
 
 - Usé `print("Hola mundo")` y funcionó bien.
 - Probé `print('Ricardo')` y también funcionó.
 - Intenté `print(Ricardo)` sin comillas y dio error porque no existe la variable.
 - Escribí `print"Ricardo"` y `print'Ricardo'` sin paréntesis y ambos
-![alt text](../fundamentos%20python/img/seccion1.png)
+  ![alt text](../fundamentos%20python/img/seccion1.png)
 
 ## Laboratorio 2 - La función print() y sus argumentos
 
 - El primer print pone "Programming" y no da salto de línea, por eso el siguiente sale pegado.
-- El segundo print pone "Essentials" y le suma "***in..." al final, todo en la misma línea.
+- El segundo print pone "Essentials" y le suma "\*\*\*in..." al final, todo en la misma línea.
 - El último print pone "Python" y ahora sí hace salto de línea porque es el valor por defecto.
 - Así
-![alt text](../fundamentos%20python/img/seccion1_2.png)
+  ![alt text](../fundamentos%20python/img/seccion1_2.png)
+
+## Laboratorio 3 - Dando formato a la salida
+
+- Usé solo un print con varios saltos de línea `\n` para dibujar la flecha, así el código es más corto.
+- Probé agrandar la flecha usando más espacios y asteriscos, manteniendo el mismo estilo.
+- Puse dos flechas una junto a la otra, solo duplicando el patrón.
+- Si quito las comillas, Python marca error porque no sabe qué imprimir.
+- Si quito los paréntesis, también da error porque en Python los print necesitan paréntesis.
+- Si escribo `Print` con mayúscula, Python no lo reconoce porque distingue entre mayúsculas y minúsculas.
+- Usar comillas simples o dobles funciona igual, mientras estén
+![alt text](img/seccion1_3.png)
---
+++ source/seccion 1/Dando formato a la salida.py
@@ -0,0 +1,63 @@
+print("    *\n"
+      "   * *\n"
+      "  *   *\n"
+      " *     *\n"
+      "***   ***\n"
+      "  *   *\n"
+      "  *   *\n"
+      "  *****")
+# En vez de usar muchos print(), todo lo meto en uno solo con saltos de línea \n.
+print("        **")
+print("       *  *")
+print("      *    *")
+print("     *      *")
+print("********  ********")
+print("     *      *")
+print("     *      *")
+print("     ********")
+# Agrando todo, pero mantengo el mismo estilo. Solo uso más espacios y más asteriscos.
+print("    *     *")
+print("   * *   * *")
+print("  *   * *   *")
+print(" *     *     *")
+print("***   *****   ***")
+print("  *   *   *   *")
+print("  *   *   *   *")
+print("  *****   *****")
+#Pongo dos flechas una junto a la otra, en la misma línea.
+print(    *)      
+print(   * *)     
+print(  *   *)     
+print( *     *)     
+print(***   ***)     
+print(  *   *)     
+print(  *   *)     
+print(  *****)
+#Si quito las comillas, Python no sabe qué imprimir lo que da error de sintaxis.
+print "    *"
+print "   * *"
+print "  *   *"
+print " *     *"
+print "***   ***"
+print "  *   *"
+print "  *   *"
+print "  *****"
+#En Python, los print deben tener paréntesis, si no, da error de sintaxis.
+Print("    *")
+Print("   * *")
+Print("  *   *")
+Print(" *     *")
+Print("***   ***")
+Print("  *   *")
+Print("  *   *")
+Print("  *****")
+# Python distingue mayúsculas y minúsculas, así que Print no sirve para mostrar algun mensaje.
+print('    *')
+print('   * *')
+print('  *   *')
+print(' *     *')
+print('***   ***')
+print('  *   *')
+print('  *   *')
+print('  *****')
+# Todo funciona igual mientras use las comillas bien cerradas.
